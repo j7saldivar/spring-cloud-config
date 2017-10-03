4e6cd4f3a60b4a89bf04533cb264255d17486017
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2017-10-02 21:52:57 -0500

    Added RefreshScope and documentation

--- spring-cloud-config-eureka-client/src/main/java/com/jorgesaldivar/controller/MainConfigController.java
+++ spring-cloud-config-eureka-client/src/main/java/com/jorgesaldivar/controller/MainConfigController.java
@@ -2,12 +2,22 @@ package com.jorgesaldivar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jorgesaldivar.config.ClientConfig;
 
+/**
+ * Calling http://localhost:8080/refresh will manually refresh the configuration properties.
+ * {@link RefreshScope} works for {@link Value} and {@link Bean}
+ * 
+ * @author saldivar
+ *
+ */
 @RestController
+@RefreshScope
 public class MainConfigController {
 
 	private ClientConfig clientConfig;
